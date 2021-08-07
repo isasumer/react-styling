9fe3f89fc20bb911cad26a496765d32d2ce47ab0
isa <sumerisa5@gmail.com>
2021-08-07 12:54:06 +0300

    imported sassy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import Inline from "./components/inline/Inline"
-import StyleSheet from "./components/regular-style/StyleSheet"
+// import Inline from "./components/inline/Inline"
+// import StyleSheet from "./components/regular-style/StyleSheet"
+import Sassy from "./components/sassy/Sassy"
 
 
 const App = () => {
   return (
-    <div>
-      <Inline />
-      <StyleSheet primary = {false}/>
+    <div>                                
+      {/* <Inline />
+      <StyleSheet primary = {false}/> */}
+      <Sassy />
     </div>
   );
 };
