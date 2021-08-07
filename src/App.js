ccc55f1f7eeb0b1bac96bf0497e2d8c4edc57f5e
isa <sumerisa5@gmail.com>
2021-08-07 12:26:34 +0300

    regulaar css added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Inline from "./components/inline/Inline"
+import StyleSheet from "./components/regular-style/StyleSheet"
 
 
 const App = () => {
   return (
     <div>
       <Inline />
+      <StyleSheet primary = {true}/>
     </div>
   );
 };
--- src/components/inline/Inline.js
+++ src/components/inline/Inline.js
@@ -1,14 +1,14 @@
-const myStyles = {
-  par : {
-    color:"blue",
-    fontSize:"1.2rem",
-    fontWeight:"bolder"
-  },
-  secondDiv: {
-    backgroundColor:"#b0b0b0"
-  }
-}
-
+// const myStyles = {
+//   par : {
+//     color:"blue",
+//     fontSize:"1.2rem",
+//     fontWeight:"bolder"
+//   },
+//   secondDiv: {
+//     backgroundColor:"#b0b0b0"
+//   }
+// }
+import {myStyles} from "./styles"
 const Inline = () => {
   return (
     <div style={{backgroundColor:"red", color:"#fff"}}>
